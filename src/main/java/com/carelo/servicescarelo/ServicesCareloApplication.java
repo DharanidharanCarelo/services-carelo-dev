4bb6c39c89dc84f50b0b14b460bb5245343031c7
Dharanidharan Palanisamy <108048897+DharanidharanPalanisamy@users.noreply.github.com>
2023-09-27 22:12:41 +0530

    Initial project setup changes

---
+++ src/main/java/com/carelo/servicescarelo/ServicesCareloApplication.java
@@ -0,0 +1,13 @@
+package com.carelo.servicescarelo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicesCareloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicesCareloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carelo/servicescarelo/ServicesCareloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carelo.servicescarelo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicesCareloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
